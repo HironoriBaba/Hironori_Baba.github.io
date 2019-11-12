c6c65ad4aae13b5c7f767184bdff143a1349d10e
Ryan Tong <rrtong@ucdavis.edu>
2019-11-11 17:14:47 -0800

    smooth jquery 500ms scrolling

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ function showInfo(projNumber) {
     hideInfo();
     document.querySelector('#projectInfo').style.display = 'block';
     document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display = 'block';
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $("#projectInfo").offset().top
+    }, 500);
   } else {
     document.querySelector('#projectInfo').style.display = 'none';
     document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display = 'none';
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $("#projects").offset().top
+    }, 500);
   }
 }
 
