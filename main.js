77b88a91aa301f0ab4204ceafd2ee0315da6fced
Ryan Tong <rrtong@ucdavis.edu>
2019-11-11 19:15:25 -0800

    first click now opens project info

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function showInfo(projNumber) {
-  if (document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display === 'none') {
+  if (document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display === 'block') {
+    document.querySelector('#projectInfo').style.display = 'none';
+    document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display = 'none';
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $("#projects").offset().top
+    }, 500);
+  } else {
     hideInfo();
     document.querySelector('#projectInfo').style.display = 'block';
     document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display = 'block';
     $([document.documentElement, document.body]).animate({
       scrollTop: $("#projectInfo").offset().top
-    }, 500);
-  } else {
-    document.querySelector('#projectInfo').style.display = 'none';
-    document.querySelector(`#projectInfo #proj${projNumber}Info`).style.display = 'none';
-    $([document.documentElement, document.body]).animate({
-      scrollTop: $("#projects").offset().top
     }, 500);
   }
 }
